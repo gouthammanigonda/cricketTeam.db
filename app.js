498256303b72bb5316f09e998bbd4b59a21bb83d
Goutham Manigonda <105107344+gouthammanigonda@users.noreply.github.com>
2022-05-12 20:16:47 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -26,6 +26,15 @@ const initializeDBAndServer = async () => {
 };
 initializeDBAndServer();
 
+let convertDBObjToResponseObj = (DBObj) => {
+  return {
+    playerId: dbObject.player_id,
+    playerName: dbObject.player_name,
+    jerseyNumber: dbObject.jersey_number,
+    role: dbObject.role,
+  };
+};
+
 //GET Players API:
 app.get("/players/", async (request, response) => {
   const getPlayersQurey = `SELECT * FROM cricket_team`;
@@ -44,10 +53,42 @@ app.post("/players/", async (request, response) => {
 
 //GET Player API:
 app.get("players/:playerId/", async (request, response) => {
-  const { playerID } = request.param;
+  const { playerId } = request.params;
+  console.log(playerId);
   const getPlayerQuery = `
     SELECT * FROM cricket_team WHERE player_id = ${playerId}
     `;
   const player = await db.get(getPlayerQuery);
-  response.send(player);
+  response.send(convertDBObjToResponseObj(player));
 });
+
+//PUT Player API:
+app.put("/players/:playerId/", async (request, response) => {
+  const { playerName, jerseyNumber, role } = request.body;
+  const { playerId } = request.params;
+  const updatePlayerQuery = `
+  UPDATE cricket_team
+  SET 
+    player_name = '${playerName}',
+    jersey_number = ${jerseyNumber},
+    role = '${role}'
+  WHERE 
+   player_id = ${playerId};
+  `;
+  await db.run(updatePlayerQuery);
+  response.send(`Player Details Updated`);
+});
+
+//DELETE Player API:
+app.delete("/players/:playerId/", async (request, response) => {
+  const { playerId } = request.params;
+  const deletePlayerQuery = `
+  DELETE FROM
+    cricket_team
+  WHERE
+    player_id = ${playerId};`;
+  await database.run(deletePlayerQuery);
+  response.send("Player Removed");
+});
+
+module.exports = app;
